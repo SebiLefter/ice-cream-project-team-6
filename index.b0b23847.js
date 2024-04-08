6258544a4e47aa4c55173c638f83da7860ef494a
SebiLefter <158606166+SebiLefter@users.noreply.github.com>
2024-04-08 16:49:59 +0000

    Deploying to gh-pages from @ SebiLefter/ice-cream-project-team-6@2b5ccfea439c65b56e1d6d35772d2c0d749afa36 🚀

---
+++ index.b0b23847.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-about-open]"),closeModalBtn:document.querySelector("[data-about-close]"),modal:document.querySelector("[data-about]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.b0b23847.js.map
