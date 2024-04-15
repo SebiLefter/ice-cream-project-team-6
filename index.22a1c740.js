b832fb1c1b6af9c4f221ef0d758b46903a020225
SebiLefter <158606166+SebiLefter@users.noreply.github.com>
2024-04-15 16:35:46 +0000

    Deploying to gh-pages from @ SebiLefter/ice-cream-project-team-6@4c64622491414e53a9dfab36155ad0d4dd7c53b0 🚀

---
+++ index.22a1c740.js
@@ -0,0 +1,2 @@
+var modal=document.getElementById("myModal"),btn=document.getElementById("description-modal"),span=document.getElementsByClassName("close")[0];btn.onclick=function(){modal.style.display="block"},span.onclick=function(){modal.style.display="none"},window.onclick=function(n){n.target==modal&&(modal.style.display="none")};
+//# sourceMappingURL=index.22a1c740.js.map
